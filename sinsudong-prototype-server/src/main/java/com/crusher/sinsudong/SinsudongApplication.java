906524529d7abb6fe36b23832d84eed3878bdf71
ywoo21 <ywooo21@gmail.com>
2019-09-19 15:31:30 +0900

    Rename Project Folder

---
+++ sinsudong-prototype-server/src/main/java/com/crusher/sinsudong/SinsudongApplication.java
@@ -0,0 +1,13 @@
+package com.crusher.sinsudong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinsudongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinsudongApplication.class, args);
+    }
+
+}
